ccb232d150f0f05fd1df433a92152ce2f3a88536
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-14 23:18:40 +0530

    Add files via upload

---
+++ Basics/Divison.java
@@ -0,0 +1,8 @@
+public class Divison {
+    public static void main(String[] args) {
+        int a = 563;
+        int b = 456;
+        double r = a/b;
+        System.out.println(r);
+    }
+}
---
+++ Basics/Substraction.java
@@ -0,0 +1,8 @@
+public class Substraction {
+    public static void main(String[] args) {
+        int t = 6;
+        int y = 5;
+        int r = t-y;
+        System.out.println(r);
+    }
+}
