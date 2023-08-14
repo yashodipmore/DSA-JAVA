e2f3904c5f3c94a92e3a393a99c6419159ad22bd
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-14 23:12:59 +0530

    Add files via upload

---
+++ Basics/Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 60;
+        int result = a+b;
+        System.out.println(result);
+    }
+}
