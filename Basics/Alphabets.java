9a129be51319cf9793b3a0c8ae87541ba9a67496
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-18 12:49:39 +0530

    Add files via upload

---
+++ Basics/Alphabets.java
@@ -0,0 +1,11 @@
+class Alphabets
+{
+    public static void main(String args[])
+    {
+        char ch;
+        for (ch ='a';ch <='z';ch++)
+        {
+            System.out.println(ch);
+        }
+    }
+}
