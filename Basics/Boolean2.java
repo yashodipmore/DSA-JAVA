db47cf18f25eab9cee099df517849f6743314b3d
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-18 16:15:13 +0530

    Add files via upload

---
+++ Basics/Boolean2.java
@@ -0,0 +1,9 @@
+public class Boolean2 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        int z = 7;
+        boolean result = (x < y || y > z);
+        System.out.println(result);
+    }
+}
