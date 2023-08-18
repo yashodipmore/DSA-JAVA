be1ecf643ca5b15b8297d65f5600c82158d70c0e
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-18 16:15:33 +0530

    Add files via upload

---
+++ Basics/Boolean.java
@@ -0,0 +1,9 @@
+public class Boolean {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+        int c = 3;
+        boolean result = (a>b && b<c);
+        System.out.println(result);
+    }
+}
