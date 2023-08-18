9aef5d8f1fd315f62712183cb344a8ffcecc1d99
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-18 12:50:36 +0530

    Add files via upload

---
+++ Basics/IP.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class IP {
+    public static void main(String[] args) throws Exception{
+        System.out.println(InetAddress.getLocalHost());
+    }
+}
