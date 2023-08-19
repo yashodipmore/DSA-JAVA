5eb9c5a915631fd5cf4c51561b5063ceeb1df04b
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-20 01:57:52 +0530

    Add files via upload

---
+++ Basics/First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello These is My First JAVA Program");
+        int Age = 19;
+        System.out.println("My Name is YASHODIP MORE , I am "+Age+" Old");
+    }
+}
