9942274759cc576df377de3a568e2b819f624f71
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-19 16:15:24 +0530

    Add files via upload

---
+++ Basics/Variable_avg.java
@@ -0,0 +1,10 @@
+class Variable_avg
+{
+    public static void main(String[] args) {
+        int a = 234;
+        int b = 576;
+        int c = 546;
+        int average = (a+b+c)/3;
+        System.out.println(average);
+    }
+}
