96fc13ccec9578610b4cdcfda38c1afaec526603
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-20 01:58:08 +0530

    Add files via upload

---
+++ Basics/Multi_Comments.java
@@ -0,0 +1,18 @@
+public class Multi_Comments {
+    public static void main(String[] args) {
+        /*public is access Modifires
+         static is mwthod
+        void does not return anything
+        Then pass the arguments in the form of String*/
+        String name = "Yashodip More";
+        /*name is the variable of String Data Type*/
+        int PRN = 221105061;
+        /*PRN is variable of integer data type*/
+        int RollNo = 021;
+        /*RollNo is the Data type integer*/
+        String Div = "EE";
+        /*Div is the Data Type of String*/
+        System.out.println(name + "\n"+ PRN +"\n"+ RollNo +"\n"+Div);
+        /*THe line is use to Print the output on the screen*/
+    }
+}
