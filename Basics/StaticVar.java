9746c9ef4585e7104608fe09f494ac77286d61be
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-21 23:25:06 +0530

    Add files via upload

---
+++ Basics/StaticVar.java
@@ -0,0 +1,13 @@
+public class StaticVar {
+    
+    public static String name = "Yashodip More";
+    public static int n = 21;
+
+    public static void main(String[] args) {
+        
+        StaticVar v = new StaticVar();
+
+        System.out.println("My name is "+v.name);
+        System.out.println("My roll no is "+v.n);
+    }
+}
