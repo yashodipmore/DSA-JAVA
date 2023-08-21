2bf316577c82df9a308a7fcba08fe25d58b652a6
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-21 23:22:53 +0530

    Add files via upload

---
+++ Basics/DtataType.java
@@ -0,0 +1,30 @@
+class DataType
+{
+    public static void main(String[] args) {
+        String s = "Yashodip More";
+        System.out.println(s);
+
+        int age = 19;
+        int RollNo = 21;
+        float f = 2.4f;
+        double d = 23.56677;
+        char c = 'Y';
+        short S = 23;
+        long l = 833768545;
+        byte v = 5;
+
+        boolean b = (age > RollNo);
+
+        System.out.println(age);
+        System.out.println(RollNo);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(s);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(v);
+        System.out.println(b);
+
+
+    }
+}
