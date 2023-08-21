9029f3cb561bf63d1d27573144747638938135ae
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-21 23:24:43 +0530

    Add files via upload

---
+++ Basics/LocalVar1.java
@@ -0,0 +1,14 @@
+public class LocalVar1 {
+    public static void main(String[] args) {
+        int X = 10;
+        if (X < 14)
+        {
+            String result = "String is less than 14";
+            System.out.println(result);
+        }
+        else
+        {
+            System.out.println("10 greater than 14");
+        }
+    }
+}
