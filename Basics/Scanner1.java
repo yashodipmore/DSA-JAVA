b6a7df7afffca10f0dfe26988817bfcf5d883585
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-25 23:32:42 +0530

    Add files via upload

---
+++ Basics/Scanner1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Scanner1{
+    public static void main(String args[])
+    {
+      Scanner sc = new Scanner (System.in);
+      String n = sc.next();
+      System.out.println(n);
+    }
+}
