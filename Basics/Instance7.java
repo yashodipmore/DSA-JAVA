5739b860c8fac6a1602e8f9e49596a641173563f
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-08-28 09:14:11 +0530

    Add files via upload

---
+++ Basics/Instance7.java
@@ -0,0 +1,8 @@
+public class Instance7 {
+    String s ="Yashodip More";
+
+    public static void main(String[] args) {
+        Instance7 sc = new Instance7();
+        System.out.println(sc.s);
+    }
+}
