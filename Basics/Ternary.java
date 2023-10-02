31726269676ba287bbc2a70f67f7d95001a505b5
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-10-02 10:22:23 +0530

    Add files via upload

---
+++ Basics/Ternary.java
@@ -0,0 +1,7 @@
+public class Ternary {
+    public static void main(String[] args) {
+        int n = 4;
+        String result = (n%2==0) ? "even":"odd";
+        System.out.println(result);
+    }
+}
