46963db17af6e51ce2871d0bad0af0b5975991d1
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-10-03 19:58:37 +0530

    Add files via upload

---
+++ Basics/If-else/Decision2.java
@@ -0,0 +1,15 @@
+class Decision2
+{
+    public static void main(String args[])
+    {
+        double tmp = 103.5;
+        if(tmp < 100)
+        {
+            System.out.println("You dont have fever");
+        }
+        else
+        {
+            System.out.println("You have fever");
+        }
+    }
+}
