ef67390fad773de7bef7d325c78e9bb35ab0baf4
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-10-06 23:01:17 +0530

    Add files via upload

---
+++ Basics/Loops1.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class Loops1 {
+    public static void main(String agrs[])
+    {
+        int i;
+        for (i = 1; i<=5000 ;i++)
+        {
+            System.out.println("Hello JAVA");
+        }
+    }
+}
