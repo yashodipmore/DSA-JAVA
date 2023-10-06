d05ed1c8689ebd3a899034e9c7422a77b3c0b5b8
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-10-06 23:02:24 +0530

    Add files via upload

---
+++ Basics/Loops4.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class Loops4 {
+    public static void main(String args[])
+    {
+        int lines;
+        for (lines = 1; lines <= 4;lines++)
+        {
+            System.out.println("********");
+        }
+    }
+}
