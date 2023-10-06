1543c9066d843d4c8fad01dc0a061ff44b46d521
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-10-06 23:02:02 +0530

    Add files via upload

---
+++ Basics/Loops3.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class Loops2 {
+    public static void main(String agrs[])
+    {
+        int i;
+        for (i = 1; i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
