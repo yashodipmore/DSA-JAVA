7cee8015a43e75d910fb0cade63a5e60321dd834
Yashodip  More <113869613+yashodipmore@users.noreply.github.com>
2023-10-07 22:54:49 +0530

    Add files via upload

---
+++ Basics/Loops10.java
@@ -0,0 +1,16 @@
+package Loops;
+
+public class Loops10 {
+    public static void main(String args[])
+    {
+        for (int i = 1; i<=5;i++)
+        {
+            if(i==3)
+            {
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("Exit the Loop");
+    }
+}
